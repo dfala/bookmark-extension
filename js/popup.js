699110a4605249fecf81cbaf4776de0862648a64
Daniel Falabella <dnlfala@gmail.com>
2015-06-14 14:04:10 -0600

    Linking js file.

--- js/popup.js
+++ js/popup.js
@@ -1,58 +1,24 @@
-console.log("Jeff rocks!");
+$("body").append('Test');
+console.log('hi');
+alert('connected!');
 
 
-//Functions
-
-function demoTab() {
-	openTab("http://prefundia.com");
-}
-
-function openTab(tabUrl) {
-	//Open a new tab: https://developer.chrome.com/extensions/tabs#method-create
-	chrome.tabs.create({
-		url: tabUrl
-	});
-}
-
-function printBookmarks(bookmarksBar) {
-	for (var i = 0; i < bookmarksBar.children.length; i++) {
-		var child = bookmarksBar.children[i];
-
-		var bookmarkParagraphNode = $("<p></p>");
-
-		if (child.url) {
-			//It's a bookmark!
-			var bookmarkLinkNode = $("<a></a>")
-				.attr("href", child.url)
-				.text(child.title)
-				.click(function (event) {
-					event.preventDefault();
-					openTab($(this).attr("href"));
-				});
-			bookmarkParagraphNode.append(bookmarkLinkNode);
-			$("#bookmarks").append(bookmarkParagraphNode);
-		} else {
-			//It's a folder!
-			bookmarkParagraphNode.text("Folder: " + child.title);
-			$("#bookmarks").append(bookmarkParagraphNode);
-		}
-	}
-}
-
-//Initialize everything!
-
-$(document).ready(function() {
-
-	$("#demoTab").click(function() {
-		demoTab();
-	});
-
-	chrome.bookmarks.getTree(function (allBookmarks) {
-
-		var bookmarksBar = allBookmarks[0].children[0];
-		console.log("Got bookmarks!", bookmarksBar);
-
-		printBookmarks(bookmarksBar);
-	});
-
-});
+// {
+//   "name": "jQuery DOM",
+//   "version": "2",
+//   "manifest_version": 2,
+//   "description": "Manipulate the DOM when the page is done loading",
+//   "background_page": "background.html",
+//   "browser_action": {
+//     "name": "Manipulate DOM",
+//     "icons": ["icon.png"],
+//     "default_icon": "icon.png"
+//   },
+//   "permissions": [
+//     "tabs"
+//   ],
+//   "content_scripts": [ {
+//     "js": [ "jquery-2.1.0.min.js", "background.js" ],
+//     "matches": [ "http://*/*", "https://*/*"]
+//   }]
+// }
