35620ad781018868f0bf73d88deccbb835687c27
Daniel Falabella <dnlfala@gmail.com>
2015-06-14 23:40:32 -0600

    Finished extension v.1

--- js/popup.js
+++ js/popup.js
@@ -1,24 +1,8 @@
-$("body").append('Test');
-console.log('hi');
-alert('connected!');
+var iframeRoot = 'https://pelican.firebaseapp.com/#/bookmark/?=';
 
+chrome.tabs.getSelected(null,function(tab) {
+    console.log(tab);
+    var tablink = tab.url;
 
-// {
-//   "name": "jQuery DOM",
-//   "version": "2",
-//   "manifest_version": 2,
-//   "description": "Manipulate the DOM when the page is done loading",
-//   "background_page": "background.html",
-//   "browser_action": {
-//     "name": "Manipulate DOM",
-//     "icons": ["icon.png"],
-//     "default_icon": "icon.png"
-//   },
-//   "permissions": [
-//     "tabs"
-//   ],
-//   "content_scripts": [ {
-//     "js": [ "jquery-2.1.0.min.js", "background.js" ],
-//     "matches": [ "http://*/*", "https://*/*"]
-//   }]
-// }
+    $('#main-iframe').attr('src', iframeRoot + tablink);
+});
