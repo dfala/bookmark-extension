e053869f1fb4bec199a455b570c44fbcbf0d2682
Daniel Falabella <dnlfala@gmail.com>
2015-06-16 09:38:37 -0600

    Adding test site (commented out).

--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,5 @@
 var iframeRoot = 'https://pelican.firebaseapp.com/#/bookmark/?=';
+// var iframeRoot = 'http://localhost:3000/#/bookmark/?=';
 
 chrome.tabs.getSelected(null,function(tab) {
     console.log(tab);
